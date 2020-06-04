236488d99b0fbc47da2b006d10e5d91881e0e660
Romain Coupey <schaft115@hotmail.com>
2020-06-04 19:36:49 +0200

    add Create Destroy to attendances controller

--- app/controllers/attendances_controller.rb
+++ app/controllers/attendances_controller.rb
@@ -1,2 +1,16 @@
 class AttendancesController < ApplicationController
+
+  def create
+    @attendance = Attendance.new
+    @attendance.event = Event.find(params[:event_id])
+    @attendance.user_id = current_user.id
+    @attendance.save
+  end
+
+  def destroy
+    @attendance = Attendance.find(params[:id])
+    @event = @attendance.event
+    @attendance.destroy
+  end
+
 end
