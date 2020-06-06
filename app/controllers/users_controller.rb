51988abbd161317bc1b9ed1c332c6812bc4f89b5
victorgeai <victor.geai@gmail.com>
2020-06-06 13:11:16 +0200

    affichage dynamique onglets User ok

--- app/controllers/users_controller.rb
+++ app/controllers/users_controller.rb
@@ -21,6 +21,7 @@ class UsersController < ApplicationController
   def show
     sql = "user_id = #{current_user.id}"
     @events = Event.where(sql)
+    @destination = params[:destination] || "events"
   end
 
   def edit
