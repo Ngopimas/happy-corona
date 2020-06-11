245340ba0c29c5495efbe675722d9c467234f7c9
Alice Fresko <fresko.alice1@gmail.com>
2020-06-11 19:39:40 +0200

    last changes

--- app/controllers/events_controller.rb
+++ app/controllers/events_controller.rb
@@ -1,8 +1,12 @@
 class EventsController < ApplicationController
 
   def index
-    if params[:query].present?
-      @events = Event.where("title ILIKE ?", "%#{params[:query]}%")
+    if params[:search].present?
+      #@events = Event.where("title ILIKE ?", "%#{params[:search][:query]}%")
+      sql_query = " \
+        events.title @@ :query \
+      "
+      @events = Event.where(sql_query, query: "%#{params[:search][:query]}%")
     else
       @events = Event.all
     end
